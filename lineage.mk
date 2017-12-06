73920b4d255afa8dedce6e25ed382bf086432749
Coulibaly Nonta <coulibalynonta@gmail.com>
2017-12-06 13:00:20 +0000

    Y6'ify

--- lineage.mk
+++ lineage.mk
@@ -1,26 +1,26 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := namath
+PRODUCT_RELEASE_NAME := Y6
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/motorola/namath/device_namath.mk)
+$(call inherit-product, device/doogee/Y6/device_namath.mk)
 
-TARGET_SCREEN_HEIGHT := 854
-TARGET_SCREEN_WIDTH := 480
-DEVICE_RESOLUTION := 480x854
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+DEVICE_RESOLUTION := 720x1280
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := namath
-PRODUCT_NAME := lineage_namath
-PRODUCT_BRAND := Motorola
-PRODUCT_MODEL := Moto C
-PRODUCT_MANUFACTURER := Motorola
+PRODUCT_DEVICE := Y6
+PRODUCT_NAME := lineage_Y6
+PRODUCT_BRAND := DOOGEE
+PRODUCT_MODEL := DOOGEE Y6
+PRODUCT_MANUFACTURER := DOOGEE
 
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
+PRODUCT_GMS_CLIENTID_BASE := android-doogee
 
 # Available languages
 PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR ar_EG
