55cec238e0994198361031ece32df853e794b87c
Coulibaly Nonta <coulibalynonta@gmail.com>
2017-12-06 12:46:51 +0000

    Attempt for Y6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,25 +1,18 @@
 # inherit from the proprietary version
--include vendor/motorola/namath/BoardConfigVendor.mk
+-include vendor/doogee/Y6/BoardConfigVendor.mk
 
 
 # Disable NINJA
 #USE_NINJA := false
 
 # Architecture
-FORCE_32_BIT := true
+#FORCE_32_BIT := true
 
 # Platform
-TARGET_BOARD_PLATFORM := mt6737m
+TARGET_BOARD_PLATFORM := mt6750
 TARGET_NO_BOOTLOADER := true
 
 # Architecture
-ifeq ($(FORCE_32_BIT),true)
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
-else
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -35,47 +28,39 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
 TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
 TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
-endif
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := mt6737m
+TARGET_BOOTLOADER_BOARD_NAME := mt6755
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Kernel
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-TARGET_KERNEL_SOURCE := kernel/motorola/namath
-BOARD_KERNEL_BASE := 0x40000000
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
+BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_RAMDISK_OFFSET := 0x04000000
-BOARD_TAGS_OFFSET := 0xE000000
-ifeq ($(FORCE_32_BIT),true)
-ARCH := arm
-TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CONFIG := wt6737m_35_n_defconfig
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 androidboot.selinux=permissive androidboot.selinux=disabled 
-BOARD_KERNEL_OFFSET := 0x00008000
-else
+BOARD_RAMDISK_OFFSET := 0x04f88000
+BOARD_TAGS_OFFSET := 0x03f88000
+
 TARGET_KERNEL_ARCH := arm64
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive androidboot.selinux=disabled 
-BOARD_KERNEL_OFFSET = 0x00080000
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_OFFSET = 0x00008000
 TARGET_USES_64_BIT_BINDER := true
-endif
+
 BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
 
 # make_ext4fs requires numbers in dec format
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216 
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216 
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2432696320
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 4698144768
-BOARD_CACHEIMAGE_PARTITION_SIZE := 419430400
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3506438144
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 452984832
+BOARD_CACHEIMAGE_PARTITION_SIZE := 3221225472
 BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_KMODULES := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := namath,namath_54,Moto_C,Moto C,C
+TARGET_OTA_ASSERT_DEVICE := Y6,hct6750_c66_b_m,hct6750_66_n,0123456789ABCDEF,y6
 
 # Disable memcpy opt (for audio libraries)
 TARGET_CPU_MEMCPY_OPT_DISABLE := true
@@ -88,15 +73,16 @@ BOARD_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
 
 
 # Graphics
-BOARD_EGL_CFG := /vendor/motorola/namath/vendor/lib/egl/egl.cfg
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-MTK_HWC_SUPPORT := yes
-MTK_HWC_VERSION := 1.4.1
-MTK_GPU_VERSION := mali midgard r7p0
+BOARD_EGL_CFG := /vendor/doogee/Y6/vendor/lib/egl/egl.cfg
+#BOARD_EGL_WORKAROUND_BUG_10194508 := true
+#USE_OPENGL_RENDERER := true
+#NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+#TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+#TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+#MTK_HWC_SUPPORT := yes
+#MTK_HWC_VERSION := 1.4.1
+#MTK_GPU_VERSION := mali midgard r7p0
+TARGET_BOARD_PLATFORM_GPU := mali-t860mp2
 
 # Mediatek support
 BOARD_USES_MTK_HARDWARE:=true
@@ -114,7 +100,7 @@ BOARD_USES_MTK_AUDIO := true
 
 # CMHW
 BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS := device/motorola/namath/cmhw
+BOARD_HARDWARE_CLASS := device/doogee/Y6/cmhw
 
 # Fix video autoscaling on old OMX decoders
 TARGET_OMX_LEGACY_RESCALING := true
@@ -123,7 +109,7 @@ TARGET_OMX_LEGACY_RESCALING := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # RIL
-BOARD_RIL_CLASS := ../../../device/motorola/namath/ril/
+BOARD_RIL_CLASS := ../../../device/doogee/Y6/ril/
 
 # GPS
 BOARD_GPS_LIBRARIES :=true
@@ -160,17 +146,17 @@ BOARD_HAVE_BLUETOOTH := true
 #BOARD_HAVE_BLUETOOTH_MTK := true
 #BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
 BOARD_BLUETOOTH_BDROID_HCILP_INCLUDED := 0
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/motorola/namath/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/doogee/Y6/bluetooth
 
 TARGET_LDPRELOAD += mtk_symbols.so
 TARGET_LDPRELOAD += libmtk_symbols.so
 
 # CWM
-TARGET_RECOVERY_FSTAB := device/motorola/namath/rootdir/recovery.fstab
+TARGET_RECOVERY_FSTAB := device/doogee/Y6/rootdir/recovery.fstab
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # TWRP stuff
-TW_THEME := portrait_hdpi
+TW_THEME := portrait_xhdpi
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
@@ -185,19 +171,15 @@ TW_HAS_DOWNLOAD_MODE := true
 TW_EXCLUDE_SUPERSU := true
 TW_USE_TOOLBOX := true
 
-TARGET_SYSTEM_PROP := device/motorola/namath/system.prop
-TARGET_SPECIFIC_HEADER_PATH := device/motorola/namath/include
+TARGET_SYSTEM_PROP := device/doogee/Y6/system.prop
+TARGET_SPECIFIC_HEADER_PATH := device/doogee/Y6/include
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 
-ifneq ($(FORCE_32_BIT),yes)
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote32
-else
 PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64_32
-endif
 
 BOARD_SEPOLICY_DIRS := \
-       device/motorola/namath/sepolicy
+       device/doogee/Y6/sepolicy
 
 # Seccomp filter
-BOARD_SECCOMP_POLICY += device/motorola/namath/seccomp
+BOARD_SECCOMP_POLICY += device/doogee/Y6/seccomp
